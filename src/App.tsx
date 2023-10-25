a0b31dfbe3a8e1ab58ffdde30673a32e7b719933
dirtyb1rd <yeskov777.888@gmail.com>
2023-10-25 20:06:23 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styles from "./assets/Base.module.scss";
+import classes from "./assets/App.module.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <h1>APP</h1>
+      <div className={classes.navigation}></div>
+      <div className={classes.main}></div>
     </div>
   );
-}
+};
 
 export default App;
