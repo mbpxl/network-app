070b5d4f0c178a1cac0b32a2e3acd9ebab5431fc
dirtyb1rd <yeskov777.888@gmail.com>
2023-12-29 13:54:52 +0300

    created context api logic for container components: WallContainer.tsx & DialogContainer.tsx

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,16 +1,16 @@
 import classes from "./Messages.module.scss";
 import { Searchbar } from "../Profile/Searchbar/Searchbar";
 import { Contacts } from "./Contacts/Contacts";
-import { Dialog } from "./Dialog/Dialog";
 import { DialogContainer } from "./Dialog/DialogContainer";
 
 export const Messages = (props: any) => {
+  // store={props.store}
   return (
     <div className={classes.messages}>
       <Searchbar />
       <div className={classes.messages__flex}>
         <Contacts contacts={props.appState.contacts} />
-        <DialogContainer store={props.store} />
+        <DialogContainer />
       </div>
     </div>
   );
